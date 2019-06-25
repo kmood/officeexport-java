4822941a8809c55b34aeb54b72956e7ca230619e
DazzlingSun <17611104164@163.com>
2019-06-25 20:10:56 +0800

    调整核心代码

---
+++ xmltoword-java/src/main/java/com/core/word/XmlModelHandler.java
@@ -0,0 +1,12 @@
+package com.core.word;
+
+/**
+ * @Auther: SunBC
+ * @Date: 2019/6/18 11:01
+ * @Description:
+ */
+public interface XmlModelHandler {
+    public void VerifyModel(String xmlPath) throws Exception;
+    public void XmlPlaceHolderHandler(String xmlPath) throws Exception;
+    public String ConverToFreemaker(String xmlPath)throws Exception;
+}
