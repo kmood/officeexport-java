e404da66cae458baffc911ce4f77c6d3ca440474
kmood <17611104164@163.com>
2019-07-10 17:39:29 +0800

    调整数据格式处理插件，添加转换map功能

--- xmltoword-java/src/test/java/com/test/test.java
+++ xmltoword-java/src/test/java/com/test/test.java
@@ -1,9 +1,14 @@
 package com.test;
 
 
+import com.core.utils.ConvertionUtil;
+import com.core.utils.JsonBinder;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Auther: SunBC
@@ -13,8 +18,32 @@ import java.util.*;
 public class test {
     @Test
     public  void test(){
-        System.out.println("123#312#".lastIndexOf('#',"123#312#".lastIndexOf("#")-1));
-        System.out.println(this.getClass().getClassLoader().getResource("./").getPath());
+        try {
+//            System.out.println("123#312#".lastIndexOf('#',"123#312#".lastIndexOf("#")-1));
+//            System.out.println(this.getClass().getClassLoader().getResource("./").getPath());
+//            HashMap<Object, Object> map = new HashMap<>();
+//            map.put("test","test");
+//            HashMap<Object, Object> map2 = new HashMap<>();
+//            map2.put("test","test");
+//            HashMap<Object, Object> map1 = new HashMap<>();
+//            map1.put("test",new Date());
+//            ArrayList<Object> objects = new ArrayList<>();
+//            objects.add(map2);
+//            objects.add(map1);
+//            objects.add(new String[]{"1","2"});
+//
+//            map.put("list",objects);
+//            Gson gson = JsonBinder.buildNormalBinderAndHandlerMap("yyyy-MM-dd");
+//
+//            String s = gson.toJson(map);
+//
+//            Map map3 = gson.fromJson(s, new TypeToken<HashMap<String, Object>>() {
+//            }.getType());
+            System.out.println("$.lis2222t[2]222[0]2[0]".replaceAll("\\[[0-9]{1,}]","*"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 
